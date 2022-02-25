1cf090a9275a937275e138b8155bf14308a6ac8c
gribdesbois <gribdesbois@gmail.com>
2022-02-25 23:41:22 +0100

    canvascBoard

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,16 @@
-import { Provider } from "react-redux"
-import store from "./store"
+import { ChakraProvider, Container, Heading } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = () => {
-  return(
-    
+  return (
+    <Provider store={store}>
+      <ChakraProvider>
+        <Container maxW="container.lg" centerContent>
+          <Heading as="h1" size="xl"></Heading>
+        </Container>
+      </ChakraProvider>
+    </Provider>
   )
 }
 
